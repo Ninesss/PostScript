df7fcba63ad51d5ba44f761021193d19c214d764
Ninesss <36767216+Ninesss@users.noreply.github.com>
2018-04-09 18:31:56 -0400

    Add files via upload

---
+++ text.cpp
@@ -0,0 +1,13 @@
+0 0 moveto 300 400 lineto stroke
+0 0 moveto 150 0 lineto 150 150 lineto 0 150 lineto closepath
+0 0 0 setrgbcolor
+stroke
+20 20 moveto 70 20 lineto 70 70 lineto 20 70 lineto closepath
+0 0 0 setrgbcolor
+fill
+50 50 moveto 100 100 lineto 50 30 lineto closepath
+255 50 50 setrgbcolor
+stroke
+100 100 moveto 150 150 lineto 50 10 lineto closepath
+50 255 50 setrgbcolor
+fill
