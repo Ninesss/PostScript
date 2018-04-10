afca72601433e6af1cb313e134751c255a3c8526
Ninesss <623840239@qq.com>
2018-04-09 21:05:02 -0400

    debug

--- main.cpp
+++ main.cpp
@@ -7,16 +7,16 @@ private:
     ofstream ps;
     float rs, gs, bs;
     float rf, gf, bf;
-    
+
 public:
     Postscript(const char *str) {
         ps.open(str);
-        rs = 0;
-        gs = 0;
-        bs = 0;
-        rf = 0;
-        gf = 0;
-        bf = 0;
+        rs = 1;
+        gs = 1;
+        bs = 1;
+        rf = 1;
+        gf = 1;
+        bf = 1;
     }
 
     ~Postscript() {
@@ -56,10 +56,10 @@ public:
     }
 
     void grid(float a, float b, float c, float d, float e, float f){
-        ps << "/vline {" << a << " " << d << " moveto " << a << " " << f << " lineto stroke} def\n" << "gasave\n" << "vline\n";
+        ps << "/vline {" << a << " " << d << " moveto " << a << " " << f << " lineto stroke} def\n" << "gsave\n" << "vline\n";
         ps << (c-a)/b << " {" << b << " 0 translate vline} repeat\n" << "grestore\n";
-        ps << "/hline {" << a << " " << d << " moveto " << c << " " << d << " lineto stroke} def\n" << "gasave\n" << "vline\n";
-        ps << (f-d)/e << " {" << 0 << " e translate hline} repeat\n" << "grestore\n";
+        ps << "/hline {" << a << " " << d << " moveto " << c << " " << d << " lineto stroke} def\n" << "gsave\n" << "hline\n";
+        ps << (f-d)/e << " {" << 0 << " " << e <<" translate hline} repeat\n" << "grestore\n";
     }
     void drawCircle(float x, float y, float r){
         ps<<x<<" "<<y<<" "<<r<<" "<<"0 360 arc \n"<<rs<<" "<<gs<<" "<<bs<<" "<<"setrgbcolor \n"<<"stroke \n";
@@ -68,23 +68,23 @@ public:
         ps<<x<<" "<<y<<" "<<r<<" "<<"0 360 arc \n"<<rf<<" "<<gf<<" "<<bf<<" "<<"setrgbcolor \n"<<"fill \n";
     }
     void text(float x, float y, const char* str){
-        ps<<"/Times-Roman findfont 12 scalefont setfont newpath"<<x<<" "<<y<<" "<<"moveto"<<"("<<str<<")"<<rs<<" "<<gs<<" "<<bs<<" "<<"setrgbcolor \n"<<"show \n";
+        ps<<"/Times-Roman findfont 12 scalefont setfont newpath "<<x<<" "<<y<<" "<<"moveto "<<"("<<str<<") "<<rs<<" "<<gs<<" "<<bs<<" "<<"setrgbcolor \n"<<"show \n";
     }
 };
 
 int main() {
-    Postscript p("test.ps");
+    Postscript p("/Users/nines/CLionProjects/ttt/test.ps");
 
-    p.line(0,0, 300,400);  // 0 0 moveto 300 400 lineto stroke
-    int r = 50, g = 255, b = 50;
-    p.drawRect(0, 0, 150, 150);   // x y moveto x+w y lineto x+w y+h lineto ... closepath stroke
-    p.fillRect(20, 20, 50, 50);   // x y moveto x+w y lineto x+w y+h lineto ... closepath fill
-    p.setFillColor(r, g, b); // 1.0 0.5 0 setrgbcolor
+    p.line(50,50, 300,400);  // 0 0 moveto 300 400 lineto stroke
+    int r = 250, g = 150, b = 150;
     p.setStrokeColor(255, 50, 50);
-    p.drawTriangle(50, 50, 100, 100, 50, 30);
-    p.fillTriangle(100, 100, 150, 150, 50, 10);
-    p.drawCircle(150,100,50); // x y 0 360 r arc stroke
+    p.setFillColor(r, g, b); // 1.0 0.5 0 setrgbcolor
+    p.drawRect(0, 0, 300, 500);   // x y moveto x+w y lineto x+w y+h lineto ... closepath stroke
+    p.fillRect(100, 200, 200, 200);   // x y moveto x+w y lineto x+w y+h lineto ... closepath fill
+    p.drawTriangle(50, 50, 300, 300, 50, 100);
+    p.fillTriangle(100, 100, 500, 500, 50, 10);
+    p.drawCircle(0,100,500); // x y 0 360 r arc stroke
     p.text(80, 80, "Welcome, to my beloved city"); // look it up  setfont  (ABC) show
 
-    p.grid(300, 50, 500, 400, 50, 700);
+    p.grid(200, 50, 400, 300, 50, 500);
 }
